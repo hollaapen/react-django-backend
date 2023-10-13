222c7ecb3443fae01d35ff87014a40c3fc693236
Ezra <holla@apensoftwares.co.ke>
2023-10-13 10:28:36 +0300

    dsds

--- customer/urls.py
+++ customer/urls.py
@@ -5,8 +5,6 @@ urlpatterns = [
 
     path('api/customers/', views.customers, name="customers"),
     path('api/customers/<int:id>', views.Customerdetails, name="details"),
-    path('api/students', views.students, name="students"),
-    path('api/students/<int:id>', views.studentdetails, name="studentdetails")
 
 
 ]
--- customer/views.py
+++ customer/views.py
@@ -18,15 +18,58 @@ def customers(request):
     serializer = CustomerSerializer(data, many=True)
     return JsonResponse({'customers': serializer.data})
 
+# def Customerdetails(request, id):
+#     try:
+#         data = Customer.objects.get(id=id)
+#     except Customer.DoesNotExist:
+#         return redirect("/")
+#     serializer = CustomerSerializer(data)
+#     return JsonResponse({'customer': serializer.data})
+
+
+@api_view(['GET', 'POST'])
+def customers(request):
+    # invoke serializer and return to client
+    if request.method == 'GET':
+        data = Customer.objects.all()
+        serializer = CustomerSerializer(data, many=True)
+        return Response({'customers': serializer.data})
+
+    elif request.method == 'POST':
+        serializer = CustomerSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'customer': serializer.data}, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET', 'POST', 'DELETE'])
 def Customerdetails(request, id):
     try:
         data = Customer.objects.get(id=id)
     except Customer.DoesNotExist:
-        return redirect("/")
-    serializer = CustomerSerializer(data)
-    return JsonResponse({'customer': serializer.data})
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CustomerSerializer(data)
+        return Response({'customer': serializer.data})
+
+    elif request.method == 'DELETE':
+        data.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+    elif request.method == 'POST':
+
+        serializer = CustomerSerializer(data, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'customer': serializer.data})
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
 
 def students(request):
